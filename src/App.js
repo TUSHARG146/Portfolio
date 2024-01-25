336a9f7413b54b06b8c65bab0c96f18e52da7998
TUSHARG146 <20bcs9268@cuchd.in>
2024-01-26 03:10:09 +0530

    fix skill section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
@@ -22,3 +21,5 @@ function App() {
 }
 
 export default App;
+
+
